50d1fc17436476e611a0d1d931fcfef5af61119d
Ori Roitzaid <oriroitzaid@gmail.com>
2024-01-30 20:11:51 +0200

    Fixed import of AutoBuilder in RobotContainer
    Co-authored-by: drorKeren <drorKeren@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
