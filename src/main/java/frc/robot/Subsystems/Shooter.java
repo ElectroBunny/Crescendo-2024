d73559fea3c34dc3e361d3820bbcc80de17b80ed
Magshimim <ac8274@bs.amalnet.k12.il>
2024-02-08 23:18:01 +0200

    removed one control import

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -4,7 +4,6 @@
 
 package frc.robot.Subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
